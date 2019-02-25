97faec7aab577694200f26223a41b8cb90d69663
Jamie Cook <jamie.cook@veitchlister.com.au>
2019-02-25 10:42:50 +1000

    updates git aliases

--- files/.aliases/git.sh
+++ files/.aliases/git.sh
@@ -1,23 +1,14 @@
 alias gs='git status -sb'
-alias gl='git log --oneline --decorate'
-alias gl='git l'
 alias gp='git push'
 alias ga='git add'
 alias gaa='git add .'
 alias gau='git add -u'
-alias gc='git commit -v'
-alias gb='git branch -v -v'
-alias gba='gb -a'
-alias gc.="gc -m '.'"
-alias gcm="gc -m "
-alias gfo="git fetch origin --prune"
-alias gch="git checkout"
-alias gch-="git checkout -"
+alias gcm="git commit -m"
+alias gc="git checkout"
+alias gc-="git checkout -"
 
 # These are awesome. Only page when necessary.
 alias gdi='git --no-pager diff --staged --color | less -FXRS'
 alias gd='git --no-pager diff --color | less -FXRS'
 alias gdc='git diff --cached'
-
-alias gsp='git fetch origin && git rebase --autostash -p'
 
