596c12ed2b9ea69cbb7054ec6117ba3ecb1494be
Jan Zill <jan.zill@veitchlister.com.au>
2019-07-18 11:52:56 +1000

    updates repo path

--- clone_and_link.sh
+++ clone_and_link.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 cd &&
-[ -d '.dot-files' ] || git clone git://github.com/vlc/dot-files.git .dot-files &&
+[ -d '.dot-files' ] || git clone git://github.com/janzill/dot-files.git .dot-files &&
 ls -1d --color=none .dot-files/files/* .dot-files/files/.* | while read f; do
   [ "$f" == '.dot-files/files/.' ] ||
   [ "$f" == '.dot-files/files/..' ] ||
